e6bc8d3f0bd7c51ed134ea8b94ef12677827e56e
Rustem Galiullin <zux.grr@gmail.com>
2025-08-13 16:49:44 +0500

    unique constraint check fix

--- src/main/java/dev/zux13/currency/exchange/util/SQLExceptionUtils.java
+++ src/main/java/dev/zux13/currency/exchange/util/SQLExceptionUtils.java
@@ -7,13 +7,16 @@ import java.sql.SQLException;
 @UtilityClass
 public class SQLExceptionUtils {
 
+    private static final String UNIQUE_VIOLATION_MESSAGE = "UNIQUE constraint failed";
     private static final String UNIQUE_VIOLATION_SQL_STATE = "23505";
 
     public boolean isUniqueConstraintViolation(Throwable ex) {
         Throwable cause = ex;
         while (cause != null) {
             if (cause instanceof SQLException sqlEx) {
-                return UNIQUE_VIOLATION_SQL_STATE.equals(sqlEx.getSQLState());
+                String message = sqlEx.getMessage();
+                return UNIQUE_VIOLATION_SQL_STATE.equals(sqlEx.getSQLState())
+                        || (message != null && message.contains(UNIQUE_VIOLATION_MESSAGE));
             }
             cause = cause.getCause();
         }
